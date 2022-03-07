c7d99c6bb9821cc3d8df1bf866abc02fc09d2649
yiellr123 <1105471091@qq.com>
2022-03-07 17:21:16 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-2go1iipzb8154da6","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-2go1iipzb8154da6 --n quickstartFunctions --r --project "D:\wxapp\toenroll" --report_first --report
